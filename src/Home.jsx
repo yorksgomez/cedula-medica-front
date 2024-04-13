0642183fe298b60432a346b2a0b1659ff76c121e
Yorks Kevin Gomez Lopez <yorks.gomez@tdcla.com>
2024-04-12 22:23:55 -0500

    agregado scan

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import Layout from "./Layout";
 import qr from "./assets/qr.png";
 
 function Home() {
+  const navigate = useNavigate();
+  
+  const goScan = () => {
+    navigate('scan');
+  };
+
   return (
     <Layout>
         <div className="accident-section">
             <p>¿Ha ocurrido un accidente?</p>
-            <button className="accident-button">!</button>
+            <button className="accident-button" onClick={goScan}>!</button>
         </div>
         <div className="id-section">
             <div className="id-box">
--- src/Scan.jsx
+++ src/Scan.jsx
@@ -1,5 +1,28 @@
+import Layout from "./Layout";
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+import Webcam from 'react-webcam';
+
 function Scan() {
-  return <h2>Scan Page</h2>;
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+    const navigate = useNavigate();
+    delay(10000).then(() => {
+        navigate('/user');
+    });
+    const webcamRef = React.useRef(null);
+    return (
+        <Layout>
+            <div className="cam-container">
+                <Webcam
+                    audio={false}
+                    ref={webcamRef}
+                    screenshotFormat="image/jpeg"
+                    width={300}
+                    height={300}
+                />
+            </div>
+        </Layout>
+    );
 }
 
 export default Scan;
