863f502fc44ea93083c487f3dcf6db09cc9c015b
Yorks Kevin Gomez Lopez <yorks.gomez@tdcla.com>
2024-04-12 22:37:07 -0500

    cambios

--- src/User.jsx
+++ src/User.jsx
@@ -1,5 +1,31 @@
+import Layout from "./Layout";
+import user from "./assets/user.jpg";
+
 function User() {
-  return <h2>User Page</h2>;
+  return (
+    <Layout>
+        <div className="user-container">
+            <div className="core">
+                <img src={user} alt="" className="user-image" />
+                <div className="info">
+                    <h4>Nombre: John Doe</h4>
+                    <p>Edad: 20</p>
+                    <p>Padecimiento: Diabetes</p>
+                </div>
+            </div>
+            <div className="medical-history">
+                <ul>
+                    <li>Historial Medico</li>
+                    <li>Historial Medico</li>
+                    <li>Historial Medico</li>
+                    <li>Historial Medico</li>
+                    <li>Historial Medico</li>
+                    <li>Historial Medico</li>
+                </ul>
+            </div>
+        </div>
+    </Layout>
+  );
 }
 
 export default User;
